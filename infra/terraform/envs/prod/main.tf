011aae9806fdc65debb1bb23255cc71dc81e665d
alicoppe <aidanlicoppe@gmail.com>
2026-01-30 17:36:59 -0500

    Added in fixes to terraform for deployment

--- infra/terraform/envs/prod/main.tf
+++ infra/terraform/envs/prod/main.tf
@@ -43,6 +43,8 @@ resource "google_vpc_access_connector" "serverless" {
   region        = var.region
   network       = data.google_compute_network.default.name
   ip_cidr_range = "10.8.0.0/28"
+  min_instances = 2
+  max_instances = 2
 
   depends_on = [google_project_service.vpc_access_api]
 }
